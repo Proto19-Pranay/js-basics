2d6873b039aebafdde544e51beb9b3ff9bd6454a
unknown <pranaylaad456@gmail.com>
2020-09-29 10:57:49 +0530

    Variables

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 console.log("Hello from index.js");
 
-let  name="Pranay";
-console.log(name);
+let  name="Pranay"; //Variable Declaration
+console.log(name); //Printing on console
+const rateOfInterest = 0.3; // declaration of constant variable
+console.log(rateOfInterest); //printing constant variable
+
+//--------------Variables----------
+let myName = "Somename"; //String literal
+let age = 22; // Number literal
+let isApproved = true // Boolean literal
+let firstName = undefined // Default value of a literal
+let selectedColor = 'red';
+let selectedColor = null; // Explicitly clearing value from a literal
