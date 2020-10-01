0a980ddb28a437046053eae31bf67db7445ddafd
unknown <pranaylaad456@gmail.com>
2020-10-01 23:01:56 +0530

    added functionsinjs.js file

---
+++ Functionsinjs.js
@@ -0,0 +1,16 @@
+// Program to add two numbers using functions
+function addSomeValues(num1, num2)
+    {
+        return num1+num2;
+    }
+
+let result=addSomeValues(25,78.3);
+console.log(`Addition of values: ${result}`);
+
+function findSquare(num)
+{
+    return num**2;
+}
+
+let power = findSquare(7);
+console.log("Power is : "+power);
