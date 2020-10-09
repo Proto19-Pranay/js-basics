78163b48b65b618f120b1eb5ef60f841eb91caf0
unknown <pranaylaad456@gmail.com>
2020-10-09 23:48:28 +0530

    Objects

--- objects.js
+++ objects.js
@@ -14,8 +14,8 @@ const circle = {
 
 };
 
-//-----Factory Functions------
-// Produces objects
+//-----Factory Functions------  
+// Produces objects , follows camel notation
 
 function createCircle(radius) {
     return {
@@ -32,3 +32,15 @@ console.log(circle1);
 
 const circle2 = createCircle(9);
 console.log(circle2);
+
+//------Constructor Functions------- 
+// Pascal Notation (OneTwoThree)
+
+function Circle(radius){
+    this.radius = radius;
+    this.draw = function(){
+        console.log('Draw');
+    }
+}
+
+const circle = new Circle(5); // Way to create the object for a Constructor function
