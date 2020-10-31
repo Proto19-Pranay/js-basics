4935f5aba9e04d2439a50466c2c8c1039065daf8
unknown <pranaylaad456@gmail.com>
2020-10-31 21:12:31 +0530

    joining and iterating arrays

--- Arrays/Arrays1.js
+++ Arrays/Arrays1.js
@@ -1,4 +1,4 @@
-// Joining two arrays
+// Combining two arrays
 
 const first = [1,2,3];
 const second = [4,5,6];
@@ -10,8 +10,28 @@ console.log(result);
 
 const combined = [...first, 'a', ...second, 'b'];
 console.log(combined);
+const combinedarray = [...combined];
 
 // Slicing an array
 
 const slice = result.slice(2,4);
 console.log(slice);
+
+// Iterating array through forEach
+
+combinedarray.forEach((itr, index) => console.log("Index: ",index,"combined array: ",itr));
+
+// Joining arrays
+
+let link = ['www','google','com'];
+link = link.join('.');
+console.log('link: ',link);
+
+// Usage of Split and Join
+
+let querymessage = 'How to learn programming';
+let msgtoarray = querymessage.split(' ');
+console.log(msgtoarray); 
+
+const URL = msgtoarray.join('-');
+console.log('URL: ', link+'/'+URL);
